6093d93de99a7798e9e4be5c23a8b422bd5eb506
Simon Jinaphant <t-sij@microsoft.com>
2018-06-20 14:12:55 -0700

    Clean up of hiring manager

--- mongoimport/mongoimport.go
+++ mongoimport/mongoimport.go
@@ -8,6 +8,7 @@
 package mongoimport
 
 import (
+	"math"
 	"runtime"
 
 	"github.com/globalsign/mgo"
@@ -429,35 +430,37 @@ func (imp *MongoImport) importDocuments(inputReader InputReader) (numImported ui
 type AddWorkerAction func(h *HiringManager, a int)
 
 type HiringManager struct {
-	latencyRecords       []int64
-	chargeRecords        []int64
-	workerCount          int
-	collectionThroughput int
-	rateLimitCount       int64
-	wg                   *sync.WaitGroup
-	recordWg             *sync.WaitGroup
-	Action               AddWorkerAction
+	latencyRecords     []int64
+	consumptionRecords []int64
+
+	rateLimitCounter int64
+	workerCount      int
+	throughput       int
+
+	workerWg *sync.WaitGroup
+	recordWg *sync.WaitGroup
+	Action   AddWorkerAction
 }
 
-func NewHiringManager(defaultWorkers int, ru int) *HiringManager {
+func NewHiringManager(defaultWorkers int, throughput int) *HiringManager {
 	return &HiringManager{
-		latencyRecords:       make([]int64, 0, defaultWorkers),
-		chargeRecords:        make([]int64, 0, defaultWorkers),
-		workerCount:          0,
-		rateLimitCount:       0,
-		collectionThroughput: ru,
-		wg:                   new(sync.WaitGroup),
-		recordWg:             new(sync.WaitGroup),
-		Action:               nil,
+		latencyRecords:     make([]int64, 0, defaultWorkers),
+		consumptionRecords: make([]int64, 0, defaultWorkers),
+		rateLimitCounter:   0,
+		workerCount:        0,
+		throughput:         throughput,
+		workerWg:           new(sync.WaitGroup),
+		recordWg:           new(sync.WaitGroup),
+		Action:             nil,
 	}
+}
+
+func (h *HiringManager) AwaitAllWorkers() {
+	h.workerWg.Wait()
 }
 
 func (h *HiringManager) CountWorkers() int {
 	return h.workerCount
-}
-
-func (h *HiringManager) Await() {
-	h.wg.Wait()
 }
 
 func (h *HiringManager) CanNotify(workerId int) bool {
@@ -466,107 +469,100 @@ func (h *HiringManager) CanNotify(workerId int) bool {
 
 func (h *HiringManager) Notify(workerId int, latency int64, charge int64) {
 	if latency < 0 {
-		log.Logvf(log.Always, "Bad sample latency")
 		return
 	}
 	defer h.recordWg.Done()
 	atomic.StoreInt64(&h.latencyRecords[workerId], latency)
-	atomic.StoreInt64(&h.chargeRecords[workerId], charge)
-	//log.Logvf(log.Always, "Worker %d reported %d ms and %d RU", workerId, latency, charge)
+	atomic.StoreInt64(&h.consumptionRecords[workerId], charge)
 }
 
-func (h *HiringManager) Start(n int) {
+func (h *HiringManager) Start(n int, imp *MongoImport) {
 	for i := 0; i < n; i++ {
 		h.HireNewWorker()
 	}
 
 	go func() {
-		/*
-			sleepTime := 5 * time.Second
+		sleepTime := 5 * time.Second
 
-				for {
-					time.Sleep(sleepTime)
-					//TODO: Exit condition if workers crash
-					log.Logv(log.Always, "Manager is requesting updates from workers")
+		for {
+			time.Sleep(sleepTime)
+			if !imp.Alive() {
+				return
+			}
 
-					h.recordWg.Add(h.workerCount)
-					for i := 0; i < h.workerCount; i++ {
-						atomic.StoreInt64(&h.latencyRecords[i], -1)
-						atomic.StoreInt64(&h.chargeRecords[i], -1)
-					}
+			h.recordWg.Add(h.workerCount)
+			for i := 0; i < h.workerCount; i++ {
+				atomic.StoreInt64(&h.latencyRecords[i], -1)
+				atomic.StoreInt64(&h.consumptionRecords[i], -1)
+			}
+			h.recordWg.Wait()
 
-					// Await for all workers to fill in their report
-					h.recordWg.Wait()
-					log.Logv(log.Always, "Manager has updates from all workers")
+			var latencySum int64
+			var chargeSum int64
+			for i := 0; i < h.workerCount; i++ {
+				latencySum += atomic.LoadInt64(&h.latencyRecords[i])
+				chargeSum += atomic.LoadInt64(&h.consumptionRecords[i])
+			}
+			averageLatency := latencySum / int64(h.workerCount)
+			averageCharge := chargeSum / int64(h.workerCount)
+			log.Logvf(log.Always, "On average, insertions took %d (ns) and consumed %d RU", averageLatency, averageCharge)
 
-					var latencySum int64
-					var chargeSum int64
-					for i := 0; i < h.workerCount; i++ {
-						latencySum += atomic.LoadInt64(&h.latencyRecords[i])
-						chargeSum += atomic.LoadInt64(&h.chargeRecords[i])
+			amount := int(math.Ceil((float64(h.throughput) * float64(averageLatency) / 1000000.0) / float64(averageCharge)))
+			amountToHire := (amount - h.workerCount) / 2
+			log.Logvf(log.Always, "Target workers %d | Hiring %d", amount, amountToHire)
+			if amountToHire <= 0 || amountToHire > 100 || h.WasRecentlyRateLimited() {
+				break
+			}
 
-					}
-					averageLatency := latencySum / int64(h.workerCount)
-					averageCharge := chargeSum / int64(h.workerCount)
-					log.Logvf(log.Always, "Avg latency %d | avg charge: %d", averageLatency, averageCharge)
+			for i := 0; i < amountToHire; i++ {
+				h.HireNewWorker()
+			}
+			log.Logvf(log.Always, "Manager thinks we can move faster; there are now %d workers", h.workerCount)
+			sleepTime = sleepTime + (3 * time.Second)
+		}
 
-					targetAmount := (float64(h.collectionThroughput) * float64(averageLatency) / 1000000.0) / float64(averageCharge)
-					amount := int(math.Ceil(targetAmount))
-					amountToHire := (amount - h.workerCount) / 2
+		log.Logv(log.Always, "Hiring manager has stopped mass hiring; switching to single hire")
 
-					log.Logvf(log.Always, "Target workers %d | Hiring %d", amount, amountToHire)
-					if amountToHire <= 0 {
-						log.Logv(log.Always, "No need to hire anymore")
-						break
-					}
-					if amountToHire > 100 {
-						log.Logv(log.Always, "Tried to hire too many")
-						break
-					}
-					limitCount := atomic.LoadInt64(&h.rateLimitCount)
-					atomic.StoreInt64(&h.rateLimitCount, 0)
-					if limitCount > 0 {
-						log.Logvf(log.Always, "There was %d `Request rate too large` responses, no extra workers are needed", limitCount)
-						continue
-					}
-					for i := 0; i < amountToHire; i++ {
-						h.HireNewWorker()
-					}
-					log.Logvf(log.Always, "Manager thinks we can move faster; there are now %d workers", h.workerCount)
-					sleepTime = sleepTime + (3 * time.Second)
-				}
-				log.Logv(log.Always, "Hiring manager has stopped mass hiring; switching to single hire")
-		*/
 		for {
 			time.Sleep(5 * time.Second)
-			limitCount := atomic.LoadInt64(&h.rateLimitCount)
-			atomic.StoreInt64(&h.rateLimitCount, 0)
-			if limitCount > 0 {
-				log.Logvf(log.Always, "There was %d `Request rate too large` responses, no extra workers are needed", limitCount)
-				continue
-			} else {
-				h.HireNewWorker()
-				log.Logvf(log.Always, "Manager thinks we can move a bit faster; there are now %d workers", h.workerCount)
+			if !imp.Alive() {
+				return
 			}
+			if h.WasRecentlyRateLimited() {
+				continue
+			}
+
+			h.HireNewWorker()
+			log.Logvf(log.Always, "Manager thinks we can move a bit faster; there are now %d workers", h.workerCount)
 		}
 	}()
 }
 
 func (h *HiringManager) HireNewWorker() {
 	h.latencyRecords = append(h.latencyRecords, 0)
-	h.chargeRecords = append(h.chargeRecords, 0)
+	h.consumptionRecords = append(h.consumptionRecords, 0)
 	newWorkerID := h.workerCount
 	h.workerCount++
 
-	h.wg.Add(1)
+	h.workerWg.Add(1)
 	go func() {
-		defer h.wg.Done()
+		defer h.workerWg.Done()
 		h.Action(h, newWorkerID)
 	}()
 }
 
 func (h *HiringManager) NotifyRateLimit() {
-	atomic.AddInt64(&h.rateLimitCount, 1)
+	atomic.AddInt64(&h.rateLimitCounter, 1)
+}
+
+func (h *HiringManager) WasRecentlyRateLimited() bool {
+	limitCount := atomic.LoadInt64(&h.rateLimitCounter)
+	atomic.StoreInt64(&h.rateLimitCounter, 0)
+	if limitCount > 0 {
+		log.Logvf(log.Always, "There was %d `Request rate too large` responses, no extra workers are needed", limitCount)
+		return true
+	}
+	return false
 }
 
 // ingestDocuments accepts a channel from which it reads documents to be inserted
@@ -586,8 +582,8 @@ func (imp *MongoImport) ingestDocuments(readDocs chan bson.D) (retErr error) {
 			imp.Kill(err)
 		}
 	})
-	manager.Start(numInsertionWorkers)
-	manager.Await()
+	manager.Start(numInsertionWorkers, imp)
+	manager.AwaitAllWorkers()
 	return
 }
 
